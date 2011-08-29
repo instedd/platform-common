10b295b4f35c2e2a6fbd7900d0a225915fe3e323
Brian J. Cardiff <bcardiff@gmail.com>
2011-08-29 12:13:05 -0300

    tidy up style for tags. improve numeric up/down. remove sample css from theme.css

--- theme/javascripts/source/init.js
+++ theme/javascripts/source/init.js
@@ -5,6 +5,21 @@
 		$("input[type='text']").addClass("ux-text")
 		$("textarea").addClass("ux-text")
 		$("input[readonly='readonly'], textarea[readonly='readonly']").addClass("readonly");
-		$(".ux-dropdown select").addClass("styled") 			
+		$(".ux-dropdown select").addClass("styled")
+		
+		
+		$(".ux-nstep").each(function(){
+			var nstep = $(this);
+			var source = $("input[type='text']", nstep);
+			var kdown = $("<input>").attr('type','button').addClass('kdown').val('');
+			var kup = $("<input>").attr('type','button').addClass('kup').val('');
+			nstep.append(kdown).append(kup);
+			var current = function(){
+				var res = parseInt(source.val());
+				return isNaN(res) ? 0 : res;
+			};
+			kdown.click(function(){ source.val(current()-1); });
+			kup.click(function(){ source.val(current()+1); });
+		});
 	});
 })(jQuery);
