6cb3f677285366b4dbb5bcd5cbad70bfce79da96
Brian J. Cardiff <bcardiff@gmail.com>
2011-08-30 12:42:37 -0300

    force tip

--- theme/javascripts/source/init.js
+++ theme/javascripts/source/init.js
@@ -1,6 +1,6 @@
 (function($){	
 	$(function(){
-		// initialize built-in components
+		// initialize built-in components.
 		$(".ux-datepicker").datepicker();
 		$("input[type='text']").addClass("ux-text");
 		$("textarea").addClass("ux-text");
